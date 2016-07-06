a78611c42deaf66bf127c826d6f7110ec33e6dc0
Pooya Eghbali <persian.writer@gmail.com>
2016-07-06 05:38:26 +0430

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,4 +1,7 @@
 vv.play({
-  url: "http://s12.telewebion.com:1935/live-secure/tv4-300k.stream/playlist.m3u8",
-  type: "application/x-mpegURL"
+  url   : "http://s12.telewebion.com:1935/live-secure/tv4-300k.stream/playlist.m3u8",
+  type  : "application/x-mpegURL",
+  title : "شبکه ۴",
+  addon : "vv.pouya.telewebion",
+  id    : "telewebion-channel4"
 });
