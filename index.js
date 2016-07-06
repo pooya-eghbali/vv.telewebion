93171632b3a95b81cb1d6a1e7d181c6d603422e9
Pooya Eghbali <persian.writer@gmail.com>
2016-07-06 14:11:06 +0430

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ getPrevious = function(previous){
   var previous = getPrevious(sub.previous);
   var page = {
     id          : 'tele-sub',
-    title       : sub.src',
+    title       : sub.src,
     addon       : 'vv.pouya.telewebion',
     constructor : constructor,
     blur        : true,
