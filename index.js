d113da069031ec0cd654e3f1786b289592669787
Pooya Eghbali <persian.writer@gmail.com>
2016-07-07 09:55:17 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,109 +1,36 @@
-var main_page = '\
+var main_page_tpl = '\
 <ul>\
-  <li><a href="#" class="tele-submenu" data-sub="ch4m"> شبکه ۴ </a></li>\
+  {{#each channels}}\
+    <li><a href="#" class="tele-submenu" data-id="{{id}}"> {{name}} </a></li>\
+  {{/each}}\
 </ul>\
 '
+main_page_tpl = Handlebars.compile(main_page_tpl);
+addon = {}
 
-var ch4m = '\
-<ul>\
-  <li><a href="#" class="tele-play-source" data-source="ch4-q300"> شبکه چهار - کیفیت ۳۰۰ </a></li>\
-  <li><a href="#" class="tele-play-source" data-source="ch4-q500"> شبکه چهار - کیفیت ۵۰۰ </a></li>\
-</ul>\
-'
-
-var submenus = {
-  ch4m: {
-    src       : ch4m,
-    title     : "شبکه ۴",
-    previous  : "main"
-  }
-}
-
-var constructor = function(){
-  return main_page;
-}
-
-var media = {
-  "ch4-q300": {
-    url       : "http://s12.telewebion.com:1935/live-secure/tv4-300k.stream/playlist.m3u8",
-    title     : "شبکه ۴",
-    previous  : "ch4m",
-    id        : "tele-ch4-300",
-    type      : "application/x-mpegURL"
+async.waterfall([
+  function(callback) {
+    // get security token (unnecessary!)
+    $.get('http://m.s1.telewebion.com/op/op?action=getSecurityToken', function(res, err) { callback(null, res) })
   },
-  "ch4-q500": {
-    url       : "http://s12.telewebion.com:1935/live-secure/tv4-500k.stream/playlist.m3u8",
-    title     : "شبکه ۴",
-    previous  : "ch4m",
-    id        : "tele-ch4-500",
-    type      : "application/x-mpegURL"
+  function(token, callback) {
+    // get channel list
+    $.getJSON('http://www.telewebion.com/mock/channel.json?nginxcache', function(res, err) { callback(null, res, token) })
   },
-}
-
-getPrevious = function(previous){
-  if(previous === "main") {
-    return addon.main;
-  }
-  var sub = submenus[previous];
-  var constructor = function(){
-    return sub.src;
-  }
-  var previous = getPrevious(sub.previous);
-  var page = {
-    id            : 'tele-sub',
-    title         : sub.title,
-    addon         : 'vv.pouya.telewebion',
-    constructor   : constructor,
-    onAfterCreate : onSubCreate,
-    previous      : previous,
-    rtl           : true,
-  }
-  return page;
-}
-
-onMainCreate = function(){
-  $(".tele-submenu").click(function(){
-    var sub = $(this).data("sub");
-    var sub = submenus[sub];
-    var constructor = function(){
-      return sub.src;
-    }
-    var previous = getPrevious(sub.previous);
-    var page = {
-      id            : 'tele-sub',
-      title         : sub.title,
+  function(channels, token, callback) {
+    var main_page = main_page_tpl({channels: channels});
+    var constructor  = function() { return main_page };
+    var onMainCreate = function() {};
+    addon.main = {
+      id            : 'tele-main',
+      title         : 'شبکه های ایران',
       addon         : 'vv.pouya.telewebion',
       constructor   : constructor,
-      onAfterCreate : onSubCreate,
-      previous      : previous,
+      onAfterCreate : onMainCreate,
       rtl           : true,
     }
-    vv.page(page);
-  })
-}
+    
+    vv.page(addon.main);
+  }
+])
 
-onSubCreate = function(){
-  $(".tele-play-source").click(function(){
-    var source = $(this).data("source");
-    vv.play({
-      id        : media[source].id,
-      url       : media[source].url,
-      type      : media[source].type,
-      title     : media[source].title,
-      addon     : "vv.pouya.telewebion",
-      previous  : getPrevious(media[source].previous)
-    });
-  })
-}
-
-addon = {}
-addon.main = {
-  id            : 'tele-main',
-  title         : 'شبکه های ایران',
-  addon         : 'vv.pouya.telewebion',
-  constructor   : constructor,
-  onAfterCreate : onMainCreate,
-  rtl           : true,
-}
-
-vv.page(addon.main);
